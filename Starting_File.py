923f9a00a3df8e5dc026fb1cbe1a2c062fe808b9
hasadi-ha <hasanonomy@gmail.com>
2018-10-29 12:32:59 -0400

    Added uid to output and inserted warning label

--- Starting_File.py
+++ Starting_File.py
@@ -1,3 +1,10 @@
+#*********** WARNING ***********#
+# This version of the starting file has a bug
+# where adding 5 or more numbers to the end of the
+# array will make ZXCVBN crash. Unsure why.
+# It will only work if input file only has that single
+# password it wants to check. Still searching for issue!
+
 import os
 from zxcvbn import zxcvbn
 from zxcvbn.matching import add_frequency_lists
@@ -25,7 +32,7 @@ file = open(cwdInput, "r")
 hashSize = int(input("Enter amount of characters in  hash (i.e. NTLM have 30 [I think] characters in their hash): "))
 
 for line in file:
-    # print(line[y:])
+    # print(line[hashSize:])
     # Send to zxcvbn to get decryption results from
     results.append(zxcvbn(line[hashSize:], user_inputs=['irrelevant', 'irrelevant']))
 
@@ -35,10 +42,16 @@ file.close()
 # Start an output file to write to
 file = open(cwdOutput, "w+")
 
+# To make uid
+count = 0
+
 for i in results:
+    # Setup counting by adding 1 each time there is a new result to output
+    count = count + 1
     # Write to output file using each line as new password and data about it
     # Also using a comma as a delimiter for possible conversion into excel sheet
-    file.write("Password:," + i["password"].strip("\n") + "," + "Guesses Log10:," + str(i['guesses_log10']) + "," + "Score:," + str(i["score"]) + ",\n")
+
+    file.write("Password:," + i["password"].strip("\n") + "," + "Guesses Log10:," + str(i['guesses_log10']) + "," + "Score:," + str(i["score"]) + "," + "uid: ," + str(count) + ",\n")
 
 # Stop using file for good practice
 file.close()
